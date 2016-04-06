f034c982239e03ebe2fda3b576c214007e5d6324
fhdone <fhdone@fma.local>
2016-04-06 15:20:34 +0800

    init

--- MakeCall/ViewController.h
+++ MakeCall/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *dialNumber;
+@property (weak, nonatomic) IBOutlet UIButton *dailButton;
 
 @end
 
--- MakeCall/ViewController.m
+++ MakeCall/ViewController.m
@@ -17,11 +17,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    //self.dailButton.backgroundColor = [UIColor grayColor];
+    
 }
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self.dailButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+//http://stackoverflow.com/questions/15864364/viewdidappear-is-not-called-when-opening-app-from-background
+- (void)appDidBecomeActive:(NSNotification *)notification {
+    NSLog(@"did become active notification");
+    [self.dailButton sendActionsForControlEvents:UIControlEventTouchUpInside];
+}
+
+
+- (IBAction)dail:(UIButton *)sender {
+    NSString *phoneNumber = [@"tel://" stringByAppendingString:self.dialNumber.text];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:phoneNumber]];
+}
+
 
 @end
